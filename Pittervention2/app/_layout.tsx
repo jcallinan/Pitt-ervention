1135658f30790344989438d61870d7ee13af120a
Jeremy Callinan <jeremy.callinan@gmail.com>
2025-02-27 18:55:31 -0500

    Update _layout.tsx
    Fixing back button issue

--- Pittervention2/app/_layout.tsx
+++ Pittervention2/app/_layout.tsx
@@ -3,5 +3,10 @@ import React from 'react';
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <Stack>
+      {/* Optionally configure static options outside the route.*/}
+      <Stack.Screen name="index" options={{}} />
+    </Stack>
+  );
 }
